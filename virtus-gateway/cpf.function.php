94b1467bba3672b66f12b28bcb0ac7d7269a4973
PauloSouza.info <web@paulosouza.info>
2020-04-14 11:33:55 -0300

    sem validação de repetição de números

--- virtus-gateway/cpf.function.php
+++ virtus-gateway/cpf.function.php
@@ -3,7 +3,7 @@ function validaCPF($cpf) {
   $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
 
   if(strlen($cpf) != 11) return '';
-  if(preg_match('/(\d)\1{10}/', $cpf)) return '';
+  // if(preg_match('/(\d)\1{10}/', $cpf)) return '';
 
   // for ($t = 9; $t < 11; $t++) {
   //   for ($d = 0, $c = 0; $c < $t; $c++) {
