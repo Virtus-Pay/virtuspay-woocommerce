6991ea3050b672246ef9c6d8cda3b0253f347e8c
PauloSouza.info <web@paulosouza.info>
2020-04-14 11:29:41 -0300

    sem cálculo para validar o cpf

--- virtus-gateway/cpf.function.php
+++ virtus-gateway/cpf.function.php
@@ -5,14 +5,14 @@ function validaCPF($cpf) {
   if(strlen($cpf) != 11) return '';
   if(preg_match('/(\d)\1{10}/', $cpf)) return '';
 
-  for ($t = 9; $t < 11; $t++) {
-    for ($d = 0, $c = 0; $c < $t; $c++) {
-      $d += $cpf{$c} * (($t + 1) - $c);
-    }
-
-    $d = ((10 * $d) % 11) % 10;
-    if ($cpf{$c} != $d) return '';
-  }
+  // for ($t = 9; $t < 11; $t++) {
+  //   for ($d = 0, $c = 0; $c < $t; $c++) {
+  //     $d += $cpf{$c} * (($t + 1) - $c);
+  //   }
+  //
+  //   $d = ((10 * $d) % 11) % 10;
+  //   if ($cpf{$c} != $d) return '';
+  // }
 
   return $cpf;
 }
